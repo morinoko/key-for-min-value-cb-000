be9be7e5101ce653444020b5e1bd45e97a53e257
Felice Forby <feliceforby@gmail.com>
2018-04-22 07:54:18 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(name_hash)
   min_val = nil
   name_hash.each do |key, value|
     if value == nil || value < min_val
+      min_val = value
       min_key = key
     end
   end
